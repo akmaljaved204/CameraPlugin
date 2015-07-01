ccfd45de959a6f4b15992d39833b16d679c3f0fc
unknown <Akmal@.(none)>
2015-07-01 17:53:51 +0500

    change preview 1

--- src/android/CameraActivity.java
+++ src/android/CameraActivity.java
@@ -38,6 +38,7 @@ import android.view.OrientationEventListener;
 import android.view.View;
 import android.widget.CompoundButton;
 import android.widget.FrameLayout;
+import android.widget.SurfaceView;
 import android.widget.ImageButton;
 import android.widget.Button;
 import android.widget.RelativeLayout;
